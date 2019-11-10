340b54f1efb565c204799017c9fd5dca9628b94b
kambrium <kambrium@users.noreply.github.com>
2019-11-10 13:47:23 +0100

    Separate module for CLI

---
+++ source/cli.d
@@ -0,0 +1,95 @@
+module cli;
+
+import core.exception;
+import defaults;
+import dssgcore;
+import errormessages;
+import server;
+import std.conv;
+import std.format;
+import std.stdio;
+import utilities;
+
+void handleCli(string[] args)
+{
+    try
+    {
+        switch (args[1])
+        {
+            case "new":
+                if (args.length == 3)
+                {
+                    createProject(args[2]);
+                    break;
+                }
+                else
+                {
+                    writeln(invalidAmountError);
+                    break;
+                }
+            case "build":
+                if (args.length == 2)
+                {
+                    buildProject;
+                    break;
+                }
+                else
+                {
+                    writeln(invalidAmountError);
+                    break;
+                }
+            case "serve":
+                if (args.length == 2)
+                {
+                    serveProject(port);
+                    break;
+                }
+                if (args.length == 3)
+                {
+                    try
+                    {
+                        port = to!ushort(args[2]);
+                        serveProject(port);
+                        break;
+                    }
+                    catch (ConvException e)
+                    {
+                        writeln(format(invalidTypeError, args[2]));
+                        break;
+                    }
+                }
+                else
+                {
+                    writeln(invalidAmountError);
+                    break;
+                }
+            case "version":
+                if (args.length == 2)
+                {
+                    getVersion;
+                    break;
+                }
+                else
+                {
+                    writeln(invalidAmountError);
+                    break;
+                }
+            case "help":
+                if (args.length == 2)
+                {
+                    showHelp;
+                    break;
+                }
+                else
+                {
+                    writeln(invalidAmountError);
+                    break;
+                }
+            default: writeln(invalidArgumentError);
+        }
+    }
+    catch (RangeError e)
+    {
+        writeln(invalidArgumentError);
+    }
+}
