3c4672d8e458966b131c7800079a5aff2bd70556
kambrium <kambrium@users.noreply.github.com>
2021-03-13 15:26:39 +0100

    Update server component

--- source/dssg/server.d
+++ source/dssg/server.d
@@ -15,14 +15,15 @@ int serveProject(ushort port)
     writeln(startingServerMsg);
 
     auto settings = new HTTPServerSettings;
-    settings.sessionStore = new MemorySessionStore;
     settings.port = port;
     settings.bindAddresses = ["::1", "127.0.0.1"];
+    settings.accessLogToConsole = true;
 
     auto router = new URLRouter;
     router.get("*", serveStaticFiles(buildRoot));
     
-    listenHTTP(settings, router);
+    auto l = listenHTTP(settings, router);
+	scope (exit) l.stopListening();
 
     return runEventLoop();
 }
